cb442d355f53d3d88b1ec0d349d4397c9953a491
Susana Arce <ingridarce4@hotmail.com>
2018-01-23 23:35:11 -0300

    ejercicios areas

---
+++ ejericio1.js
@@ -0,0 +1,21 @@
+
+let base = 6
+let height = 7
+
+function areaTriangulo (base, height){
+	return base * height /2
+}
+console.log(`el area de un triángulo de base ${base} y  de altura  ${height} es: 
+	        ${areaTriangulo(base, height)}`)
+
+
+let lado1 = 5
+let lado2 = 3
+
+function areaRectangulo (lado1, lado2) {
+	return lado1 * lado2 
+}
+
+console.log (`el area de un rectangulo de lado1 ${lado1} por su lado2 ${lado2} es:
+	         ${areaCuadrado (lado1, lado2)}`)
+
